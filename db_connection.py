964a94ccb9cfcc579c2cd14d45514adce6dc10d4
P4radox624 <153822607+P4radox624@users.noreply.github.com>
2024-05-08 01:56:51 +0800

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,14 +1,9 @@
 import mysql.connector
 #Database credentials
 def connect():
-    #try:
-        return mysql.connector.connect(
-            host="sql.freedb.tech",
-            port="3306",
-            user="freedb_guest",
-            password="N$U22Epa3WAzH?A",
-            database="freedb_credentials"
-        )
-    #except:
-        print(ConnectionRefusedError)
-
+    return mysql.connector.connect(
+        host="localhost",
+        user="username",
+        password="password",
+        database="credentials"
+    )
